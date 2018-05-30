9cb34b22a5cbbe03f8ab8ff2afc5772c219ac48c
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-05-30 15:46:54 -0400

    Added p4

---
+++ Ch2/p4.py
@@ -0,0 +1,5 @@
+# Prompt the user for a letter and show the corresponding Unicode encoding
+
+letter = input('Enter an upper or lower case letter: ')
+print('The Unicode encoding for that letter is', ord(letter))
+
