1e75c283ab761190c4b171e0d8aac34049e380bf
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-05-31 11:34:12 -0400

    Added p5 with 2 versions

--- Ch3-Control-Structures/p5.py
+++ Ch3-Control-Structures/p5.py
@@ -15,3 +15,22 @@ while not terminate:
     except ValueError:
         print('The number of positives you entered was %s, and the number of negatives was %s.' % (numpos, numneg))
         terminate = True
+
+# Alternate version using lists
+
+terminate2 = False
+nums = []
+
+while not terminate2:
+    try:
+        num = int(input('Please enter an integer number (enter any letter to finish): '))
+        nums.append(num)
+
+    except ValueError:
+        posval = [x for x in nums if x >= 0]
+        print('The number of positive values you entered is:', len(posval))
+
+        negval = [x for x in nums if x < 0]
+        print('The number of negative values you entered is:', len(negval))
+
+        terminate2 = True
