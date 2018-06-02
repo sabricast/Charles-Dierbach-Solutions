05899812220a6f899e7f6b5ab0805b82f640f861
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-06-02 13:05:10 -0400

    Changed sum to count to avoid using built-in name

--- Ch5/p3.py
+++ Ch5/p3.py
@@ -2,9 +2,10 @@
 # and a second positive integer, m <= n, and returns how many numbers
 # between 1 and n are evenly divisible by m.
 
+
 def modCount(n, m):
-    sum = 0
+    count = 0
     for k in range(1, n + 1):
-       if k % m == 0:
-           sum = sum + 1
-    return sum
+        if k % m == 0:
+            count = count + 1
+    return count
