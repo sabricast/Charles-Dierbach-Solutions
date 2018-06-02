22af79e3401e5c83c4d0bb921f8f44716449976d
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-06-02 13:11:49 -0400

    Added p4

---
+++ Ch5/p4.py
@@ -0,0 +1,7 @@
+# Write a Python function named helloWorld that displays
+# "Hello World, my name is /name/", for any given name passed to the routine.
+
+
+def helloWorld(name):
+    output = print("Hello World, my name is", name)
+    return output
