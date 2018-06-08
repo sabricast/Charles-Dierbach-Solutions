1617784ac8887b01924600504a8b83409e74e0ef
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-06-08 17:09:55 -0400

    Added p4

---
+++ Ch8/p4.py
@@ -0,0 +1,32 @@
+# Write a Python function named countAllLetters that is given a line read from a text file and
+# returns a list containing every letter in the line and the number of times that each letter
+# appears (with upper/lower case letters counted together)
+
+
+def countAllLetters(line):
+    """ Returns a list containing every letter in line and the number of times each letter appears. """
+
+    lst = []
+    letters = []
+    new_line = line.lower()
+
+    for k in new_line:
+        if k not in letters and k != ' ':
+            count = new_line.count(k)
+            value = (k, count)
+            lst.append(value)
+            letters.append(k)
+
+    print(lst)
+    return lst
+
+# --- main
+
+
+input_file = open('insertfilehere.txt', 'r')
+line = input_file.readline()
+
+countAllLetters(line)
+input_file.close()
+
+
