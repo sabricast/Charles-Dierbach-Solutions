de8dd244a1b03c42106a45d9e16c57c7ed9df43a
Sabrina Castejon <sabrina.castejon@gmail.com>
2018-06-11 16:58:40 -0400

    Added p1

---
+++ Ch11/p1.py
@@ -0,0 +1,36 @@
+# Write both a non-recursive and recursive function that determines if a given number is even or not.
+
+# Non-recursive
+
+
+def iseven(number):
+    """ (int) -> bool
+
+    Returns whether number is even.
+
+    \>>> iseven(5) -> False
+    \>>> iseven(4) -> True
+
+    """
+    return number % 2 == 0
+
+
+# Recursive
+
+
+def iseven_r(number):
+    """ (int) -> bool
+
+    Returns whether number is even.
+
+    \>>> iseven(5) -> False
+    \>>> iseven(4) -> True
+
+    """
+    if number == 2 or number == 0:
+        return True
+    elif number >= 2:
+        number -= 2
+    else:
+        return False
+    return iseven_r(number)
